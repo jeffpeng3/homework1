7d4f09b107654291bb70263704e13c1bfc25e8d0
jeffpeng3 <jeffpeng2012@gmail.com>
2022-10-19 11:11:37 +0800

    test sumAndPrintList

--- homework1/main.c
+++ homework1/main.c
@@ -3,24 +3,36 @@
 #include <stdlib.h>
 #include <string.h>
 #ifdef __linux__
-#define strtok_s(s,tok,useless) strtok(s,tok)
-#define fopen_s(ptr,path,mode) *ptr = fopen(path,mode)
-#define strcpy_s(dest,len,src) strcpy(dest,src)
+#define strtok_s(s, tok, useless) strtok(s, tok)
+#define fopen_s(ptr, path, mode) *ptr = fopen(path, mode)
+#define strcpy_s(dest, len, src) strcpy(dest, src)
+void strrev(char *str)
+{
+	char *temp = str;
+	while (temp++)
+		;
+	temp -= 2;
+	while (str > temp)
+	{
+		*str ^= *temp;
+		*temp ^= *str;
+		*str ^= *temp;
+	}
+}
 #endif
-
 
 typedef struct _node
 {
-	char* numString;
+	char *numString;
 	uint64_t length;
-	struct _node* next;
+	struct _node *next;
 } node;
-node* loadNumFromString(node* root, node* last, char* str, int* count)
+node *loadNumFromString(node *root, node *last, char *str, int *count)
 {
-	static char* token = " ,";
-	static char* tempPtr = NULL;
-	static char* strtokBuffer = NULL;
-	node* current = NULL;
+	static char *token = " ,";
+	static char *tempPtr = NULL;
+	static char *strtokBuffer = NULL;
+	node *current = NULL;
 	if (!root)
 	{
 		root = calloc(1, sizeof(node));
@@ -69,13 +81,13 @@ node* loadNumFromString(node* root, node* last, char* str, int* count)
 	}
 	return root;
 }
-char* getStringFromFile(FILE* file)
+char *getStringFromFile(FILE *file)
 {
 	uint32_t size = 0;
 	fseek(file, 0, SEEK_END);
 	size = ftell(file);
 	fseek(file, 0, SEEK_SET);
-	char* temp = calloc((uint64_t)size + 2, sizeof(char));
+	char *temp = calloc((uint64_t)size + 2, sizeof(char));
 	if (temp)
 	{
 		fgets(temp, size + 2, file);
@@ -88,7 +100,7 @@ char* getStringFromFile(FILE* file)
 
 	return temp;
 }
-void printAllList(node* current)
+void printAllList(node *current)
 {
 	printf("%s\n", current->numString);
 	if (current->next)
@@ -96,13 +108,13 @@ void printAllList(node* current)
 		printAllList(current->next);
 	}
 }
-void printNode(node* current)
+void printNode(node *current)
 {
 	printf("%s\n", current->numString);
 }
-node* getLargestNode(node* current)
+node *getLargestNode(node *current)
 {
-	node* largest = current;
+	node *largest = current;
 	current = current->next;
 	for (; current; current = current->next)
 	{
@@ -123,15 +135,15 @@ node* getLargestNode(node* current)
 	}
 	return largest;
 }
-void duplicate(node* root)
+void duplicate(node *root)
 {
-	char* map = calloc(1024, sizeof(char));
+	char *map = calloc(1024, sizeof(char));
 	if (map)
 	{
-		for (node* last = root; root; root = last->next)
+		for (node *last = root; root; root = last->next)
 		{
 
-			for (node* i = root->next; i; i = i->next)
+			for (node *i = root->next; i; i = i->next)
 			{
 				if (root->length != i->length)
 				{
@@ -141,7 +153,7 @@ void duplicate(node* root)
 				{
 					continue;
 				}
-				node* temp = root;
+				node *temp = root;
 				last->next = root->next;
 				free(temp);
 				root = last->next;
@@ -150,20 +162,64 @@ void duplicate(node* root)
 			last = root;
 		}
 	}
-
 }
-void sumAndPrintList(node* root)
+void addToFirstNode(char *sum, char *target)
 {
-	/* not implement yet*/
+	short sumEnd = 0, targetEnd = 0, carry = 0;
+	while (!(sumEnd & targetEnd))
+	{
+		*sum -= '0';
+		if (!targetEnd)
+		{
+			*sum += *target - '0';
+			target++;
+			if (!*target)
+			{
+				targetEnd = 1;
+			}
+		}
+		carry = *sum / 10;
+		*sum = *sum % 10;
+		*sum += '0';
+		if (!sumEnd)
+		{
+			sum++;
+			if (!*sum)
+			{
+				sumEnd = 1;
+			}
+		}
+	}
+}
+void sumAndPrintList(node *root)
+{
+	node sum;
+	sum.length = 1;
+	sum.numString = calloc(2, sizeof(char));
+	sum.numString[0] = '0';
+	sum.next = NULL;
+	strrev(sum.numString);
+	for (; root; root = root->next)
+	{
+		if (sum.length <= root->length)
+		{
+			sum.numString = realloc(sum.numString, sizeof(char) * (root->length + 1));
+		}
+		strrev(root->numString);
+		addToFirstNode(sum.numString, root->numString);
+		strrev(root->numString);
+	}
+	strrev(sum.numString);
+	printf("%s", sum.numString);
 }
 
 int main()
 {
-	FILE* dataSource;
+	FILE *dataSource;
 	int count = 0;
 	fopen_s(&dataSource, "./number.txt", "r");
-	char* string = getStringFromFile(dataSource);
-	node* root = loadNumFromString(NULL, NULL, string, &count);
+	char *string = getStringFromFile(dataSource);
+	node *root = loadNumFromString(NULL, NULL, string, &count);
 	printAllList(root);
 	printf("\nthe largest is:\n");
 	printNode(getLargestNode(root));
