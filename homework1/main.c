e03fc466ef126f613cbb1d6fe59841e99162a990
jeffpeng3 <jeffpeng2012@gmail.com>
2022-10-21 13:50:55 +0800

    fix scanf_s

--- homework1/main.c
+++ homework1/main.c
@@ -6,6 +6,7 @@
 #define strtok_s(s, tok, useless) strtok(s, tok)
 #define fopen_s(ptr, path, mode) *ptr = fopen(path, mode)
 #define strcpy_s(dest, len, src) strcpy(dest, src)
+#define scanf_s(format,...) scanf(format,__VA_ARGS__)
 #define clearOutput() system("clear")
 #elif _WIN32
 #define clearOutput() system("cls")
